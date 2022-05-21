152a188a98a2279c06848d2400dccbad9cb3ebaa
Atif Hussain <67437879+atif-hu@users.noreply.github.com>
2022-05-21 15:16:44 +0530

    Add files via upload

---
+++ HouseRentalLandlord/HouseRentalLandlord/Landlord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseRentalLandlord
+{
+    public class Landlord
+    {
+        public int landlordId { get; set; }
+        public string LandlordName { get; set; }
+        public string houseDetails{ get; set; }
+        public int Rent{ get; set; }
+        public string Address{ get; set; }
+    }
+}
